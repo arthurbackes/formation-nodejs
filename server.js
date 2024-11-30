4787a965be0e40eeb75ecf88bbee380ee14d0227
nmaboge <nathan.maboge@student.uclouvain.be>
2024-11-30 16:12:14 +0100

    Installation tailwind + config projet

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler.js")
 const connectDb = require("./config/dbConnection.js")
 const dotenv = require("dotenv").config();
+
 connectDb();
+
 const app = express();
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes.js"));
@@ -17,6 +25,13 @@ app.use("/api/mylists", require("./routes/listsRoutes"));
 app.use(errorHandler);
 
 
+app.get("/", (req, res, next) => {
+    res.render("index");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
 
 
 app.listen(port, () => {
